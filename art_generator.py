0c19335f3ab1e0018768f20cee687929097c0f8d
Derrick Daniel <99546535+derrick-daniel@users.noreply.github.com>
2023-11-03 10:37:04 +0530

    Added library basics

--- art_generator.py
+++ art_generator.py
@@ -1,11 +1,17 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 def generate_art():
+
     image = Image.new(
         mode="RGB",
         size=(128, 128),
         color='black'
     )
+
+    draw = ImageDraw.Draw(image)
+    line_xy = ((0, 0), (128, 128))
+    draw.line(line_xy, fill="white")
+
     image.save("test_image.png")
 
 if __name__ == "__main__":
